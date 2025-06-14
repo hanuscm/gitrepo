eb3a2520ca82e5e482c07a5ab7ec46f852969586
hanuscm <hanuaws7869@gmail.com>
2025-06-14 12:28:44 +0530

    Update applicationstart.sh

--- complete/applicationstart.sh
+++ complete/applicationstart.sh
@@ -1,1 +1,1 @@
-java -jar /var/lib/jenkins/workspace/pipeline/gitrepo/complete/target/rest-service-complete-0.0.1-SNAPSHOT.jar --server.port=8081 &
+java -jar /var/lib/jenkins/microservicejars/rest-service-complete-0.0.1-SNAPSHOT.jar --server.port=8081 &
